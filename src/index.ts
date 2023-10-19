2967796f01d8a6ed3170435450e31f6209b8fb2d
aguelin <alvaroguerrerolinares02@gmail.com>
2023-10-19 12:14:23 +0200

    feat: Subscription y unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,34 @@ const observer:Observer<any> = {
     complete: () => console.info('Completado')
 }
 
+const intervalo$ = new Observable<number>(subscriber => {
 
+    let cont = 0;
+
+    const interval = setInterval(() => {
+        cont++;
+        subscriber.next(cont);
+        console.log(cont);
+    },1000);
+
+    return () => {
+        clearInterval(interval);
+        console.log('Intervalor destruido')
+    }
+
+})
+
+const subscription1 = intervalo$.subscribe(num => console.log('Num: ',num));
+const subscription2 = intervalo$.subscribe(num => console.log('Num: ',num));
+const subscription3 = intervalo$.subscribe(num => console.log('Num: ',num));
+
+setTimeout(() => {
+    subscription1.unsubscribe();
+    subscription2.unsubscribe();
+    subscription3.unsubscribe();
+
+    console.log("Completado");
+}, 3000);
 
 
 
